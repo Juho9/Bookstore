d20dffa4aac8135316b0c82ac64b2e6bb070e43b
GunnarSven <94739044+GunnarSven@users.noreply.github.com>
2022-10-06 16:17:05 +0300

    Bookstore: CRUD

--- src/main/java/com/example/Kirjakauppa/web/BookController.java
+++ src/main/java/com/example/Kirjakauppa/web/BookController.java
@@ -3,8 +3,11 @@ package com.example.Kirjakauppa.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.Kirjakauppa.domain.Book;
 import com.example.Kirjakauppa.domain.BookRepository;
 
 
@@ -14,10 +17,35 @@ public class BookController {
 	@Autowired
 	private BookRepository repository;
 	
+	//All books
 	@RequestMapping("/booklist")
 	public String booksToList(Model model) {
 		model.addAttribute("books", repository.findAll());
 		return "booklistpage";
 	}
 
+	
+	//Add new book
+	@RequestMapping(value="/addbook")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";
+	}
+	
+	@RequestMapping(value="/save", method = RequestMethod.POST)
+	public String saveBook(Book book) {
+		repository.save(book);
+		return "redirect:booklist";
+	}
+	
+	
+	
+	
+	
+	//Delete book
+	@RequestMapping(value="/delete/{id}", method = RequestMethod.GET)
+	public String deleteBook(@PathVariable("id") Long bookId, Model model)  {
+		repository.deleteById(bookId);
+		return "redirect:../booklist";
+	}
 }
